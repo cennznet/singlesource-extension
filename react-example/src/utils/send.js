3a9f172af10721809525c3aea4fa619e2aada691
Ermal Kaleci <ermalkaleci@gmail.com>
2019-05-02 12:37:08 +0200

    fix amount toString base

--- react-example/src/utils/send.js
+++ react-example/src/utils/send.js
@@ -12,7 +12,7 @@ const send = async (sender, receiver, amount, cb) => {
 
   const value = amountToWei(new BigNumber(amount));
   api.tx.genericAsset
-    .transfer(CENNZ_ASSET_ID, receiver, value.toString())
+    .transfer(CENNZ_ASSET_ID, receiver, value.toString(10))
     .signAndSend(sender, cb);
 }
 
